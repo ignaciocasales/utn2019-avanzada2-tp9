15e70a92bc5d8beab49d7a1454f9a343b699451f
ignaciocasales <casales.ignacio@gmail.com>
2019-11-08 19:29:27 -0300

    update doc

--- src/main/java/com/utn2019/avanzada2/tp9/controller/ProductsController.java
+++ src/main/java/com/utn2019/avanzada2/tp9/controller/ProductsController.java
@@ -5,6 +5,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import com.utn2019.avanzada2.tp9.domain.Product;
 import com.utn2019.avanzada2.tp9.service.ProductsService;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +21,15 @@ public class ProductsController {
     private final ProductsService productsService;
 
     @GetMapping
-    @ApiImplicitParam(name = "Authorization", required = true, paramType = "header")
+    @ApiImplicitParam(value = "Bearer xxx.xxx.xxx",name = "Authorization", required = true, paramType = "header")
     public List<Product> getAll(
+            @ApiParam(value = "Page index to use in conjunction with the 'size' param. Positions starts at 0.")
             @RequestParam(name = "page", required = false) Integer page,
+            @ApiParam(value = "Page size to use in conjunction with the 'page' param.")
             @RequestParam(name = "size", required = false) Integer size,
+            @ApiParam(value = "Allowed values: ASC & DESC.", defaultValue = "ASC")
             @RequestParam(name = "direction", required = false) String direction,
+            @ApiParam(value = "Field to order by.", defaultValue = "productId")
             @RequestParam(name = "orderBy", required = false) String orderBy
     ) {
         if (page == null || size == null) {
